1a0f06b01cd6d9537b89122048ab692ea57fa755
antoebtfr <antoebt@hotmail.com>
2020-11-18 21:25:01 +0100

    initial commit'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomepageComponent},
+  { path: 'vocabulary', loadChildren: () => import('./pages/vocabulary/vocabulary.module').then(m => m.VocabularyModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
